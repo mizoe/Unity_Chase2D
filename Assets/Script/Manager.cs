ed28c3b0bb8241547016f7decca3a978ad2e6bba
Tomo Mizoe <mz@july.co.jp>
2014-11-10 22:04:59 +0900

    First version

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	// Player prefab
+	public GameObject enemy;
+	public GameObject player;
+	private GameObject _enemy;
+	private float enemyVel = 2f;
+	// Use this for initialization
+	void Start () {
+		InstantiateEnemy ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("_enemy:" + _enemy.transform.position);
+		print ("player:" + player.transform.position);
+		//vector2 enemy > player
+		Vector2 dir = -1 * _enemy.transform.position + player.transform.position;
+		print ("dir:" + dir.ToString());
+		_enemy.rigidbody2D.velocity = dir.normalized * enemyVel; //normalize the direction vector, and get the velocity vector
+	}
+
+	public void InstantiateEnemy () {
+		_enemy = Instantiate (enemy, enemy.transform.position, enemy.transform.rotation) as GameObject;
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// LR
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		// Up down
+		float y = Input.GetAxisRaw ("Vertical");
+
+		// moving direction
+		Vector2 direction = new Vector2 (x, y).normalized;
+		
+		// limit
+		Move (direction);
+	}
+
+	// move the player
+	void Move (Vector2 direction)
+	{
+		// 画面左下のワールド座標をビューポートから取得 get lower left (in world coordinate) from ViewPort
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		
+		// 画面右上のワールド座標をビューポートから取得 get upper right (in world coordinate) from ViewPort
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+		
+		// プレイヤーの座標を取得 get the player position
+		Vector2 pos = transform.position;
+		// 移動量を加える add displacement
+		pos += direction * this.speed * Time.deltaTime;
+		
+		// プレイヤーの位置が画面内に収まるように制限をかける not to over from the view
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+		
+		// 制限をかけた値をプレイヤーの位置とする set the player position as limited position calclated above
+		transform.position = pos;
+	}
+
+}
